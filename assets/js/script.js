e5868cfe887bac1e9f0033212f6072e7a8bd30f8
JackDay94 <Jack94day@gmail.com>
2022-06-28 16:11:31 +0000

    Add basic JS functions for game

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+function gameSelect() {
+
+}
+
+function runGame() {
+
+}
+
+function runTimeAttack() {
+
+}
+
+function difficultySelect() {
+
+}
+
+function runCountdown() {
+
+}
+
+function timer() {
+
+}
+
+function increaseTime() {
+
+}
+
+function generateWord() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function increaseScore () {
+
+}
+
+function highScore () {
+
+}
+
+function mainMenu() {
+
+}
