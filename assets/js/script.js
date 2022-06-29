bf4d0a7ec140d9f2d41f5c4bda2d774335933337
JackDay94 <Jack94day@gmail.com>
2022-06-29 18:58:37 +0000

    Add event listeners to buttons on main menu and difficulty select

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,30 +3,39 @@ function runGame() {
 }
 
 function runTimeAttack() {
-    document.getElementById("main-menu").hidden = true;
+    const timeAttackButton = document.getElementById("btn-ta")
 
-    let timeAttackHeading = document.getElementById("game-heading");
-    timeAttackHeading.textContent = "Time Attack";
-    timeAttackHeading.style.color = "cadetblue";
+    timeAttackButton.addEventListener("click", function() {
+        document.getElementById("main-menu").hidden = true;
 
+        let timeAttackHeading = document.getElementById("game-heading");
+        timeAttackHeading.textContent = "Time Attack";
+        timeAttackHeading.style.color = "cadetblue"; }
+    )
 }
 
 function difficultySelect() {
-    document.getElementById("main-menu").hidden = true;
+    const difficultySelectButton = document.getElementById("btn-cd")
 
-    let difficultySelectHeading = document.getElementById("game-heading");
-    difficultySelectHeading.textContent = "Choose a Difficulty:";
+    difficultySelectButton.addEventListener("click", function() {
+        document.getElementById("difficulty-select").hidden = false;
+        document.getElementById("main-menu").hidden = true;
 
-    document.getElementById("difficulty-select").innerHTML =  `
-    <div id='difficulty-buttons'>
-        <button type='button' id='btn-easy' class='btn-main btn-difficulty'>Easy</button>
-        <button type='button' id='btn-med' class='btn-main btn-difficulty'>Medium</button>
-        <button type='button' id='btn-hard' class='btn-main btn-difficulty'>Hard</button>
-    </div>
-    <div id='menu-button'>
-        <button type='button' id='btn-menu' class='btn-main'>Main Menu</button>
-    </div>`
-}
+        let difficultySelectHeading = document.getElementById("game-heading");
+        difficultySelectHeading.textContent = "Choose a Difficulty:";
+
+        document.getElementById("difficulty-select").innerHTML =  `
+        <div id='difficulty-buttons'>
+            <button type='button' id='btn-easy' class='btn-main btn-difficulty'>Easy</button>
+            <button type='button' id='btn-med' class='btn-main btn-difficulty'>Medium</button>
+            <button type='button' id='btn-hard' class='btn-main btn-difficulty'>Hard</button>
+        </div>
+        <div id='menu-button'>
+            <button type='button' id='btn-menu' class='btn-main' onclick="mainMenu()">Main Menu</button>
+        </div>
+        `
+    })
+};
 
 function runCountdown() {
 
@@ -57,5 +66,11 @@ function highScore () {
 }
 
 function mainMenu() {
+    const mainMenuButton = document.getElementById("btn-menu");
 
+    mainMenuButton.addEventListener("click", function() {
+        document.getElementById("main-menu").hidden = false;
+        document.getElementById("difficulty-select").hidden = true;
+        document.getElementById("game-heading").innerText = "Are you prepared to test your typing skills?";
+    })
 }
